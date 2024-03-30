cfa7cd42329684b442fda049f3c36c5e53823b4f
Will <wromov@yahoo.com>
2024-03-30 11:57:12 -0400

    Updating TDA and looping_cash_final comments for clarity.

--- TDA_script_example.py
+++ TDA_script_example.py
@@ -47,74 +47,81 @@ def chain_request(symbol):
     data = response.json()
     return data
 
-'''
-The nested for loop below:
-1. Iterate over each symbol in the tickers dataframe and requests the associated options chain data from TDA's API
-2. Seperate the calls and puts into seperate objects
-3. Create a list of all call expirations and put expirations
-4. Iterate over each expiration date in the callindex and defines the "tablename" variable based on the current expiration being iterated over (expiration dates are the psql table names)
-5. Create a list of all strikes for the current expiration being iterated over and creates dataframe object "arr" with the list of strikes as its index
-6. Create a new dataframe with columns defined by variable "c" created earlier and iterate through each strike
-7. Pull the values corresponding to "c" for the current expiration and each strike being iterated over and append those values to the dataframe
-8. Append today's date to the final column 'Today' and move on to the next strike
-9. Once all strikes are iterated over, the dataframe takes "tablename" and either creates a psql table if it doesn't already exist or appends the existing table if it does exist
-	and buckets the dataframe into the psql table that matches the expiration date being iterated over from step 4
-10. Repeats steps 4-9 for all put expirations
-'''
-
 start = time.time()
+# Iterate over each symbol in the tickers dataframe and requests the associated options chain data from TDA's API
 for i in range(0, len(tickers.index)):
 	symbol = tickers['Symbol'].iloc[i]
 	print(symbol)
 	data = chain_request(symbol)
+	# Seperate the calls and puts into seperate objects
 	calls = data['callExpDateMap']
 	puts = data['putExpDateMap']
 
+	# Create a list of all call expirations
 	callindex  = []
 	for x in calls:
 	    callindex.append(x)
 
+	# Create a list of all put expirations
 	putindex = []
 	for x in puts:
 		putindex.append(x)
 
+	# Iterate over each expiration date in the callindex and define the "tablename" variable based on the current expiration being iterated over (expiration dates are the psql table names)
 	for i in range(0,len(callindex)):
 	    expiration = callindex[i]
 	    tablename = expiration[:10]
 	    
+		# Create a list of all strikes for the current expiration being iterated over and creates dataframe object "arr" with the list of strikes as its index
 	    strikes = []
 	    for x in calls[callindex[i]]:
 	        strikes.append(x)
 
 	    arr = pd.DataFrame(data=np.array(strikes))
 
+		# Create a new dataframe with columns defined by variable "c" created earlier and iterate through each strike
 	    df=pd.DataFrame(data=None,columns=c)
+		# Pull the values corresponding to "c" for the current expiration and each strike being iterated over and append those values to the dataframe
 	    for i in range(0, len(arr)):
 	        strike = arr[0].iloc[i]
 	        values = calls[expiration][f'{strike}']
 	        df = df.append(pd.DataFrame(data=values,columns=c,index=[symbol]))
+			#8a. Append today's date to the final column 'Today' and move on to the next strike
 	        df['Today'] = today
+
+	# 	Once all strikes are iterated over, the dataframe takes "tablename" and either creates a psql table if it doesn't already exist or appends the existing table if it does exist
+	# and buckets the dataframe into the psql table that matches the expiration date being iterated over from step 4
+			
 	    sqlengine = create_engine('postgresql+psycopg2://######:######@######:######/######')
 	    dbConnection = sqlengine.connect()
 	    frame = df.to_sql(tablename, dbConnection, if_exists='append');
 	    dbConnection.close()
 
+	# Iterate over each expiration date in the putindex and define the "tablename" variable based on the current expiration being iterated over (expiration dates are the psql table names)
 	for i in range(0,len(putindex)):
 	    expiration = putindex[i]
 	    tablename = expiration[:10]
 	    
+		# Create a list of all strikes for the current expiration being iterated over and creates dataframe object "arr" with the list of strikes as its index
 	    strikes = []
 	    for x in puts[putindex[i]]:
 	        strikes.append(x)
 
 	    arr = pd.DataFrame(data=np.array(strikes))
 
+		# Create a new dataframe with columns defined by variable "c" created earlier and iterate through each strike
 	    df=pd.DataFrame(data=None,columns=c)
+		# Pull the values corresponding to "c" for the current expiration and each strike being iterated over and append those values to the dataframe
 	    for i in range(0, len(arr)):
 	        strike = arr[0].iloc[i]
 	        values = puts[expiration][f'{strike}']
 	        df = df.append(pd.DataFrame(data=values,columns=c,index=[symbol]))
+			# Append today's date to the final column 'Today' and move on to the next strike
 	        df['Today'] = today
+
+		# 	Once all strikes are iterated over, the dataframe takes "tablename" and either creates a psql table if it doesn't already exist or appends the existing table if it does exist
+	# and buckets the dataframe into the psql table that matches the expiration date being iterated over
+
 	    sqlengine = create_engine('postgresql+psycopg2://######:######@######:######/######')
 	    dbConnection = sqlengine.connect()
 	    frame = df.to_sql(tablename, dbConnection, if_exists='append');
